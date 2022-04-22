e4874ab87007c0f7f29521ba92e7bd788e41fbea
Lewis Hudson <93501740+PythonLewisH@users.noreply.github.com>
2022-04-23 10:51:59 +1200

    contact section edits

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function myMap() {
   var mapProp = {
-    center: new google.maps.LatLng(-41.286461, 174.776230),
+    center: new google.maps.LatLng(-41.301600, 174.797000),
     zoom: 15,
   };
   var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
